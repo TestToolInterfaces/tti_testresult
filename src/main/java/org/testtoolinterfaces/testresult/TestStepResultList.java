5c12e72ae6546d88775a8d8b5592efe927afc4db
arjankranenburg <Arjan.Kranenburg@gmail.com>
2012-11-17 20:58:33 +0100

    New class for a list of TestStepResults

---
+++ src/main/java/org/testtoolinterfaces/testresult/TestStepResultList.java
@@ -0,0 +1,12 @@
+package org.testtoolinterfaces.testresult;
+
+import java.util.ArrayList;
+
+public class TestStepResultList extends ArrayList<TestStepResult> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3698536176076570429L;
+
+}
